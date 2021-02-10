494e74a6aa775671eb36bbf16a6ddfee46889427
Amoory001 <57963967+Amoory001@users.noreply.github.com>
2021-02-10 12:43:54 +0100

    Add files via upload

---
+++ database_1.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+from sqlite3 import Error
+
+def sql_connection():
+
+    try:
+
+        con = sqlite3.connect('mydatabase.db')
+
+        return con
+
+    except Error:
+
+        print(Error)
+        
+#creates a cursor object to execute the create table statement
+        
+def sql_table(con):
+
+    cursorObj = con.cursor()
+
+    cursorObj.execute("CREATE TABLE employees(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)")
+
+    con.commit()                    #The commit() method saves all the changes we make
+
+con = sql_connection()
+
+sql_table(con)
